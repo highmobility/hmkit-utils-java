a722131acafe751d4766e30042ca11ad05749f44
tonis <tonisives@gmail.com>
2021-03-08 09:14:42 +0700

    fix build warnings

--- src/test/java/com/highmobility/utils/ByteUtilsTest.kt
+++ src/test/java/com/highmobility/utils/ByteUtilsTest.kt
@@ -21,10 +21,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package com.highmobility.utils;
+package com.highmobility.utils
 
 import org.junit.Assert.assertTrue
-import org.junit.Test;
+import org.junit.Test
 
 class ByteUtilsTest {
     @Test
